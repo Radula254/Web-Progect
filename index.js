72226068152b7e7e1a0352258e7f7cb2ae778940
Jayden883 <radulajoshua@gmail.com>
2023-04-24 10:20:16 +0300

    First page

---
+++ index.js
@@ -0,0 +1,7 @@
+const PI = 3.14159;
+let radius;
+let circumfrence;
+radius = window.prompt("Enter a Radius");
+radius = Number(radius);
+circumfrence = 2*PI*radius;
+console.log("The circumfrence is:",circumfrence);
